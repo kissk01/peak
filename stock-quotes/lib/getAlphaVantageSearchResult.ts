7311fa130bbc37bab3bc41d3c0e0bd85008ecaf4
Kiss Karol <karol.kiss@foolproof.co.uk>
2023-05-22 16:59:35 +0200

    Search result query creation,navbar,search component creation.

---
+++ stock-quotes/lib/getAlphaVantageSearchResult.ts
@@ -0,0 +1,7 @@
+export default async function getAlphaVantageSearchResult(searchTerm: string) {
+  const searchQuery = process.env.NEXT_PUBLIC_ALPHA_VANTAGE_SEARCH_QUERY;
+
+  const searchResponse = await fetch(`${searchQuery}${searchTerm}`);
+
+  return searchResponse.json();
+}
